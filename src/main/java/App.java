5be45f47d8bc1ba852d4183397a70106ec33537c
Natalia Telpukhova and Samuel Gespass <natalia@>
2018-01-02 10:08:22 -0800

    add frontend

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.HashMap;
+
 public class App{
     public static void main(String[] args){
-
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter a word: ");
+            String scrabbleWord = bufferedReader.readLine();
+            ScrabbleScorer myScrabble = new ScrabbleScorer();
+            System.out.println(myScrabble.calculateScore(scrabbleWord));
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
