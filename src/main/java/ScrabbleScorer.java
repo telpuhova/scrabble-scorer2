18ba151b1665c5bf7c267f358ceb42f5274f7554
telpuhova <telpuhova@gmail.com>
2018-03-20 19:46:23 -0700

    Update ScrabbleScorer.java

--- src/main/java/ScrabbleScorer.java
+++ src/main/java/ScrabbleScorer.java
@@ -9,32 +9,7 @@ public class ScrabbleScorer {
 
     public Integer calculateScore(String input) {
         char[] word = input.toLowerCase().toCharArray();
-//        scores.put('a', 1);
-//        scores.put('b', 3);
-//        scores.put('c', 3);
-//        scores.put('d', 2);
-//        scores.put('e', 1);
-//        scores.put('f', 4);
-//        scores.put('g', 2);
-//        scores.put('h', 4);
-//        scores.put('i', 1);
-//        scores.put('j', 8);
-//        scores.put('k', 5);
-//        scores.put('l', 1);
-//        scores.put('m', 3);
-//        scores.put('n', 1);
-//        scores.put('o', 1);
-//        scores.put('p', 3);
-//        scores.put('q', 10);
-//        scores.put('r', 1);
-//        scores.put('s', 1);
-//        scores.put('t', 1);
-//        scores.put('u', 1);
-//        scores.put('v', 4);
-//        scores.put('w', 4);
-//        scores.put('x', 8);
-//        scores.put('y', 4);
-//        scores.put('z', 10);
+
         Character[] arr1 = {'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'};
         Character[] arr2 = {'d', 'g'};
         Character[] arr3 = {'b', 'c', 'm', 'p'};
